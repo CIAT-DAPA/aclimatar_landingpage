8991228be18c06901706fb58198e448f400513ea
Danielaigm <danigarci852@gmail.com>
2024-01-10 12:09:52 -0500

    modals

--- src/box/modals/Modal11.jsx
+++ src/box/modals/Modal11.jsx
@@ -1,14 +1,40 @@
-
+import { useState } from 'react';
+import './style.css'
 const Modal11 = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+      setModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalOpen(false);
+    };
 
     
-      return (
+    return (
         <div className="div11" >
-                <span className="div11container__text">
+                <span onClick={openModal} className="div11container__text">
                     DEFORESTATION-FREE PRODUCTION
-                </span>                
-            </div>
-      );
+                </span> 
+
+
+                {modalOpen && (
+                    <div className="modal11-container">
+                    <div className="modal11-content">
+                        <p>Community-led deforestation alert monitoring is an effective approach to halting deforestation.
+                        Rewarding communities with outcome-based payments for ecosystem services incentivizes reporting
+                        and intervention against illegitimate deforestation. In our approach, locally calibrated real-time
+                        monitoring informs local forest guards. </p>
+                        <button className="close-button11" onClick={closeModal}>
+                        X
+                        </button>
+                    </div>
+                    </div>
+                )}
+                            
+        </div>
+    );
 };
 
 export default Modal11;
