1d4cba3940a5fb83851eb38178fd33d42eb1c4ce
Danielaigm <danigarci852@gmail.com>
2024-01-16 16:48:16 -0500

    add modals 9,5,6,16

--- src/box/modals/Modal9.jsx
+++ src/box/modals/Modal9.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
+import './style.css'
+
 const Modal9 = () => {
 
-    
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+      setModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalOpen(false);
+    };
     return (
         <div className="div9">
-             <a href="https://adaptation.aclimatar.org/" target="blank"> 
+            <a onClick={openModal} href="https://adaptation.aclimatar.org/" target="blank"> 
                 <span className="div9container__text">
                 Africa
                 </span>
             </a>
+       
+
+            {modalOpen && (
+            <div className="modal9-container">
+                <div className="modal9-content">
+                <p>Contenido del modal</p>
+                {/* <img src="./webs/modal9.PNG" alt="" /> */}
+                <button className="close-button9" onClick={closeModal}>
+                    Cerrar Modal
+                </button>
+                </div>
+            </div>
+            )}
         </div>
     
     );
