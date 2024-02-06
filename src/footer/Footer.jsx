9ec15ce126dfd4506abcbf58330f716f5c3ddf4f
Danielaigm <danigarci852@gmail.com>
2024-02-05 21:46:26 -0500

    add footer mobile

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,12 +3,21 @@ function Footer() {
  
 
     return (
+      <>
+
       <div className="footerContain">
         <p>
           CONTACT: <span>c.bunn@cgiar.org</span>
         </p>
-        
       </div>
+      <div className='mobileFooter'>
+        <img src="./logos/eia.svg" alt="" />
+        <img className='imgCiat' src="./logos/ciat.svg"  alt="" />
+       
+     </div>
+      </>
+      
+      
     )
   }
   
