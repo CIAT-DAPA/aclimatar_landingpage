e1720a013845f29766d8ec276c3a6c189b92b458
Chris1234567899 <cerlic@web.de>
2024-09-18 11:58:43 -0500

    updated gh-pages implementation

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //base: '/aclimatar_landingpage/', //use for default gh-pages
+  base: '/', // use for gh-pages with custom domain
   plugins: [react()],
 })
